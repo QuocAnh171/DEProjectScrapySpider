57ca6f05e3f5a53fdd98a91c539fdc437a322592
QuocAnh171 <74640069+QuocAnh171@users.noreply.github.com>
2024-10-09 16:27:25 +0700

    new_update

--- newegg_data/dqa171_crawl.py
+++ newegg_data/dqa171_crawl.py
@@ -1,28 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import json
+import pandas as pd
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-def crawl_data(date_time: str):
+def crawl_data_with_time(date_time: str):
     headers = {}
     x = requests.get(f'https://www.minhngoc.com.vn/ket-qua-xo-so/mien-bac/{date_time}.html', headers=headers)
 
     soup = BeautifulSoup(x.content, 'html.parser')
     content = soup.find("div", attrs={"class": "box_kqxs"})
-    rows = list()
-    for row in content.findAll("tr"):
-        rows.append(row)
+    rowkqxsdt = content.findAll("tr")[0]
 
-    rowkqxsdt = rows[0]
+    return rowkqxsdt
+
+def get_data_by_regex(rowkqxsdt, date_time):
 
     list_search = {"db", '1', '2', '3', '4', '5', '6', '7'}
     list_result = list()
-    priceItem = {}
     listPriceValue = []
 
     for i in list_search:
@@ -41,18 +41,41 @@ def crawl_data(date_time: str):
                 listPriceValue.append(str(num)[-2:])
 
     listPriceValue.sort()
-    priceItem[date_time] = result = ','.join(map(str, listPriceValue))
-    list_result.append(priceItem)
+    list_result.append({
+            'cob_dt' : date_time,
+            'list_result' : ','.join(map(str, listPriceValue))
+            
+        })
 
     return list_result
 
-def main_request():
-    start_date = date(2024, 9, 1)
-    end_date = date(2024, 9, 10)
+def list_result_from_list_time():
+    list_result = []
+    start_date = date(2024, 9, 8)
+    end_date = date(2024, 9, 11)
     for single_date in daterange(start_date, end_date):
-        a = single_date.strftime("%d-%m-%Y")
-        print(a)
-        print(crawl_data(a))
+        cob_dt = single_date.strftime("%d-%m-%Y")
+        crawl_data = crawl_data_with_time(cob_dt)
+        result = get_data_by_regex(crawl_data, cob_dt)
+        list_result.append(result)
+
+        # Chuyển danh sách thành DataFrame để dễ làm việc
+    df = pd.DataFrame(list_result)
+    return df
+
+def result_from_date_time():
+
+    cob_dt = datetime.now() - timedelta(days=1)
+    cob_dt = cob_dt.strftime("%d-%m-%Y")
+    print(cob_dt)
+    crawl_data = crawl_data_with_time(cob_dt)
+    print(crawl_data)
+    result = get_data_by_regex(crawl_data, cob_dt)
+    print(result)
+
+    # Chuyển danh sách thành DataFrame để dễ làm việc
+    df = pd.DataFrame(result)
+    return df
 
 def load_data_to_db_using_insert_value(connection, table_name, data_field, list_data):
     # step 1: check table_name exists
@@ -83,3 +106,7 @@ def load_data_to_db_using_json_file(connection, table_name, data_field, list_dat
             query_sql = """ insert into json_table
                 select * from json_populate_recordset(NULL::json_table, %s) """
             cur.execute(query_sql, (json.dumps(data),))
+
+
+if __name__ == "__main__":
+    result_from_date_time()
